6d1003abe7f0fb08d244da6a88f01f665f2d8b46
Widop <contact@widop.com>
2016-05-13 11:04:53 +0200

    Fix deploy

--- app/config/capifony/prod.rb
+++ app/config/capifony/prod.rb
@@ -1,7 +1,6 @@
+set :domain,      "app.plateau-urbain.com"
+set :deploy_to,   "/var/www/plateau-urbain"
 set :branch, "master"
-#set :domain,      "www.#{application}.fr"
-set :domain,      '188.166.72.35'
-set :deploy_to,   "/var/www/#{application}/prod"
 role :web, domain
 role :app, domain, :primary => true
 role :db,  domain, :primary => true
