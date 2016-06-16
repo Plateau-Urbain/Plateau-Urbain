3cc421b3956860e4dce5849ca1dfad7e26fad12e
Widop <contact@widop.com>
2016-06-16 14:45:30 +0200

    [HOTFIX] error surface negative

--- src/AppBundle/Entity/Parcel.php
+++ src/AppBundle/Entity/Parcel.php
@@ -27,7 +27,7 @@ class Parcel
      *
      * @ORM\Column(name="surface", type="integer")
      * @Assert\NotBlank()
-     * @Assert\Range(min = 0, minMessage = "Vous devez obligatoirement renseigner une surface positive.")
+     * @Assert\Range(min = 0, minMessage = "Vous devez obligatoirement renseigner une surface positive.", groups={"draft", "save"})
      */
     private $surface;
 
