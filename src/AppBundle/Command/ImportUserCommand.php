ffe32aa4a723c4e682b1cdeecfdaf811cc2871bd
Thomas Bernard <thomas@cls.to>
2016-11-14 19:50:58 +0100

    open CSV file and read it

--- src/AppBundle/Command/ImportUserCommand.php
+++ src/AppBundle/Command/ImportUserCommand.php
@@ -3,18 +3,35 @@ namespace AppBundle\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
+
 
 class ImportUserCommand extends Command
 {
     protected function configure()
     {
-        // ...
+        $this->setName('app:importusers')->setDescription('Import Users from CSV files');
+	$this->setHelp("TODO...");
+	$this->addArgument('csv', InputArgument::REQUIRED, 'CSV file to import');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // ...
+        $csvfile = $input->getArgument('csv');
+	$output->writeln('CSV Import : '.$csvfile);
+	$f = fopen($csvfile, "r");
+	if($f === FALSE) {
+		$output->writeln('ERROR can not open '.$csvfile);
+		return FALSE;
+	} else {
+		$headers = fgetcsv($f);       // delimiter ,
+		while(!feof($f)) {
+			$a = fgetcsv($f);	// delimiter ,
+			$output->writeln(print_r($a, true));
+		}
+		fclose($f);
+	}
     }
 }
 
