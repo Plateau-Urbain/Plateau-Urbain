0540e89f3df6244dc5e09861000b6bbe8bf9eb4a
Thomas Bernard <thomas@cls.to>
2016-11-14 23:00:24 +0100

    Save user

--- src/AppBundle/Command/ImportUserCommand.php
+++ src/AppBundle/Command/ImportUserCommand.php
@@ -22,7 +22,8 @@ class ImportUserCommand extends ContainerAwareCommand
 	// get FOSUserBunder User Manager
 	// http://symfony.com/doc/master/bundles/FOSUserBundle/user_manager.html
 	$userManager = $this->getContainer()->get('fos_user.user_manager');
-	$output->writeln(get_class_methods($userManager));
+	//$output->writeln(get_class_methods($userManager));
+	$useTypeRepo = $this->getContainer()->get('doctrine')->getManager()->getRepository('AppBundle:UseType');
         $csvfile = $input->getArgument('csv');
 	$output->writeln('CSV Import : '.$csvfile);
 	$f = fopen($csvfile, "r");
@@ -36,45 +37,80 @@ class ImportUserCommand extends ContainerAwareCommand
 		$a = fgetcsv($f);	// delimiter ,
 		$email = trim($a[4]);
 		if($email == '') continue;
-		$output->writeln(print_r($a, true));
+		//$output->writeln(print_r($a, true));
 		$user = $userManager->findUserByEmail($email);
 		if(is_null($user)) {
 			$user = $userManager->createUser($email);
+			$user->setTypeUser(0);	// 0 : porteur de projet
 			// Horodateur
-			// Civilité
-			// Nom
-			// Prénom
-			// Email
-			// Date de naissance
-			// Téléphone
-			// Présentez-vous
-			// Type d'usage
-			// Présentation de votre projet
-			// Durée d'occupation
-			// Surface en m²
-			// Date de disponibilité souhaitée
-			// Nom de votre structure
-			// Statut
-			// Date de création
-			// Adresse
-			// Code postal
-			// Ville
-			// Site web
-			// Facebook
-			// Twitter
-			// Linkedin
-			// Instagram
-			// Viadeo
-			// Google +
-			// Acceptez-vous de recevoir la newsletter de l'association?
-
-			//$userManager->updateUser($user);
+			$user->setCivility($a[1]);	// Civilité
+			$user->setLastName($a[2]);	// Nom
+			$user->setFirstName($a[3]);	// Prénom
+			$user->setEmail($email);	// Email
+			//$user->setDateOfBirth(new \DateTime(str_replace('/','-',$a[5])));	// Date de naissance
+			//$user->setDateOfBirth(ImportUserCommand::convDate($a[5]));	// Date de naissance
+			$user->setBirthDay(ImportUserCommand::convDate($a[5]));	// Date de naissance
+			$phone = trim($a[6]);		// Téléphone
+			if(strlen($phone) == 9 && $phone[0] != '0') $phone = '0'.$phone;
+			else if(substr($phone, 0, 2) == 33) $phone = '+'.$phone;
+			$user->setPhone($phone);
+			//$output->writeln($phone);
+			$user->setDescription($a[7]);	// Présentez-vous
+			$useTypeStr = trim($a[8]);	// Type d'usage
+			$u = $useTypeRepo->findOneByName($useTypeStr);
+			if(!is_null($u)) $user->setUseType($u);
+			else $output->writeln("use type $useTypeStr not found");
+			$user->setProjectDescription($a[9]);	// Présentation de votre projet
+			$duree = $a[10];	// Durée d'occupation
+			foreach(explode(' ',$duree) as $item) {
+				if(is_numeric($item)) $user->setUsageDuration(intval($item));
+				else switch(strtolower($item)) {
+				case 'an':
+				case 'ans':
+					$user->setLengthTypeOccupation('ans');
+					break;
+				case 'mois':
+					$user->setLengthTypeOccupation('mois');
+					break;
+				}
+			}
+			$surface = $a[11];	// Surface en m²
+			foreach(explode(' ',$surface) as $item) {
+				if(is_numeric($item)) $user->setWishedSize(intval($item));
+			}
+			$user->setUsageDate(ImportUserCommand::convDate($a[12]));	// Date de disponibilité souhaitée
+			$user->setCompany($a[13]);	// Nom de votre structure
+			$user->setCompanyStatus($a[14]);// Statut
+			$user->setCompanyCreationDate(ImportUserCommand::convDate($a[15]));	// Date de création
+			$user->setAddress($a[16]);	// Adresse
+			//$user->setAddressSuite()
+			$user->setZipcode($a[17]);	// Code postal
+			$user->setCity($a[18]);		// Ville
+			$user->setCompanySite($a[19]);	// Site web
+			$user->setFacebookUrl($a[20]);	// Facebook
+			$user->setTwitterUrl($a[21]);	// Twitter
+			$user->setLinkedinUrl($a[22]);	// Linkedin
+			$user->setInstagramUrl($a[23]);	// Instagram
+			//$user->setViadeo// Viadeo
+			$user->setGoogleUrl($a[25]);	// Google +
+			$newsletter = strtoupper(substr($a[26],0,1)); // Acceptez-vous de recevoir la newsletter de l'association?
+			$user->setNewsletter($newsletter != 'N');
+			//$output->writeln(get_class_methods($user));
+			$user->setPlainPassword(md5(time().rand()));
+			$userManager->updateUser($user);
 		} else {
 			$output->writeln("User already in base : $email");
 		}
-		break;
+		break;	// STOP AFTER 1ST USER
 	}
 	fclose($f);
+    }
+
+    static function convDate($str)
+    {
+	$d = explode('/', $str);
+	if(count($d) == 0) return NULL;
+	return new \DateTime(implode('/', array_reverse($d)));
     }
 }
 
