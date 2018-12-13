2f18606accede764a9ea1c0e76a4395ae1c839a2
Thomas Bernard <thomas@cls.to>
2018-12-13 11:42:58 +0100

    improve app_dev.php and add my IP to it

--- web/app_dev.php
+++ web/app_dev.php
@@ -9,10 +9,32 @@ use Symfony\Component\Debug\Debug;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
+$ip = $_SERVER['REMOTE_ADDR'];
+if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+  $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+/* debug :
+  echo "<b>_SERVER</b><br>\n";
+  foreach($_SERVER as $k => $v) {
+    echo $k . ' = ' . $v;
+    echo "<br>\n";
+  }
+  echo "<b>_REQUEST</b><br>\n";
+  foreach($_REQUEST as $k => $v) {
+    echo $k . ' = ' . $v;
+    echo "<br>\n";
+  }
+  echo "<b>apache headers:</b><br>\n";
+  foreach(apache_request_headers() as $k => $v) {
+    echo "$k : $v<br>\n";
+  }
+*/
+/*
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
 ) {
+*/
+if (!in_array($ip, array('127.0.0.1', 'fe80::1', '::1', '***REMOVED***'))) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
