950e454b56eae2036577d578c9e7a802c49569c4
Gabriel Poma <gpoma@24eme.fr>
2023-11-03 14:45:38 +0100

    on remet old password (qui faisait planter l'enregistrement)

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -36,6 +36,11 @@ class UserType extends AbstractType
                 'widget' => 'choice', 'attr' => ['class' => 'oneline-date']
             ])
             ->add('description', TextareaType::class, array('label' => "Une courte description de moi", 'attr' => array('class' => 'form-control', 'rows' => 5)))
+            ->add('oldPassword', PasswordType::class, [
+                    'mapped' => false, 'required' => false,
+                    'label' => "Mot de passe actuel",
+                    'attr' => array('class' => 'form-control')
+                ])
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'Les deux champs doivent être identique',
